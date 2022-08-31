fd07d593425368f71ddc64893721039ccf30aa8f
Kirill Sytkov <kirill.sytkov@yandex.ru>
2022-08-31 19:16:55 +0300

    feat: fix bag

--- YourWeather/AppDelegate.swift
+++ YourWeather/AppDelegate.swift
@@ -4,14 +4,19 @@ import Firebase
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        FirebaseApp.configure()
-        return true
-    }
-
+   
+   var window: UIWindow?
+   var deviceOrientation = UIInterfaceOrientationMask.portrait
+   
+   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+      
+      FirebaseApp.configure()
+      return true
+   }
+   
+   func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+      return deviceOrientation
+   }
+   
 }
 
